c37dcc91405440e8a6adf11306434709174c4676
ICT1 <08250273.sce@rub.edu.bt>
2026-03-18 14:01:11 -0500

    This is the firts class of python

---
+++ string_ex.py
@@ -0,0 +1,10 @@
+print("Hello Wolrd")
+a=1
+b=2
+c=a+b
+print(type(c))#Daya type is integer
+c=str(c)
+print(type(c))#Data type is string
+multiputlines= """This is a multipe  line string.
+This is the second line."""
+print(multiputlines)
