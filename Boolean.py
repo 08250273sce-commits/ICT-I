124904a42cd1d214a27fbede9cae2402f2019866
ICT1 <08250273.sce@rub.edu.bt>
2026-03-19 20:19:19 -0500

    Lesson on Boolean

---
+++ Boolean.py
@@ -0,0 +1,8 @@
+a=bool(0)#False
+b=bool(1)#True
+print(type(a))#Data type of a is bool
+print(type(b))#Data type of b is bool
+a=bool("")#False
+b=bool("Hello")#True
+print(type(a))#Data type of a is bool
+print(type(b))#Data type of b is bool
