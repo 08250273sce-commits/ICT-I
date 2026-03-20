584f6aa2302428dba9b5ef56cbef2df3f21b12a1
ICT1 <08250273.sce@rub.edu.bt>
2026-03-19 20:08:36 -0500

    Lesson on Float.py

---
+++ Float.py
@@ -0,0 +1,8 @@
+x = 1.10
+y=1.0
+z=35.59
+Full_mark=100.0
+print(type(Full_mark))#Data type of Full_mark is float
+Full_mark=float(Full_mark)#Type casting
+print(type(Full_mark))#Data type of Full_mark is float
+print(Full_mark)        
