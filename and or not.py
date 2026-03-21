148e8d96246e2d4f43cb97ebecdb2ec289892e9e
ICT1 <08250273.sce@rub.edu.bt>
2026-03-20 19:52:02 -0500

    And or not

---
+++ and or not.py
@@ -0,0 +1,8 @@
+x=True
+y=False
+result_and=x and y#false (both x and y are mot true)
+print("Logical AND:",result_and)
+result_or=x or y#True (X is true)
+print("Logical OR:",result_or)
+result_not=not x#False(_X is True,not false)
+print("Logical NOT:",result_not)
