002627cfdfd2b174917984a023b31bf5e4570c4a
ICT1 <08250273.sce@rub.edu.bt>
2026-03-20 19:09:42 -0500

    Arithmetic operation

---
+++ Aritmetic.py
@@ -0,0 +1,14 @@
+a=10
+b=5
+additio_result=a+b#10+5=15
+subtraction_result=a-b#10-5=5
+multiplicatipion_result=a*b#10*5=50
+division_result=a/b#10/5=2.0(division always returns a float)
+exponentiation_result=a**b#10^5=100000
+modulus_result=a%b#10%5=0(remainder of 10 divided by 5 is 0)
+print("Addition:",additio_result)
+print("Subtraction:",subtraction_result)
+print("Multiplication:",multiplicatipion_result)
+print("Division:",division_result)
+print("Exponentiation:",exponentiation_result)
+print("Modulus:",modulus_result)
