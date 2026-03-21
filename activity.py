0b6f15b6f963b4f6f5556f1146c1a753109815a1
ICT1 <08250273.sce@rub.edu.bt>
2026-03-20 20:21:27 -0500

    Activity

---
+++ activity.py
@@ -0,0 +1,12 @@
+Name=input("Enter your name:")
+print("Hello Mr/Ms",Name)
+num1=float(input("Enter the number 1:"))
+num2=float(input("Enter the number 2:"))
+add_tion=num1+num2
+print("The sum is:", add_tion)
+sub_traction=num1-num2
+print("The difference is:", sub_traction)
+mul_tiplication=num1*num2
+print("The product is:", mul_tiplication)
+div_ision=num1/num2
+print("The quotient is:",div_ision)
