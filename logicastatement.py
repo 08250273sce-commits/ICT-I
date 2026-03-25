b60e8eb9584292e1f7be4819ba33f7f5a2aa3dbd
ICT1 <08250273.sce@rub.edu.bt>
2026-03-25 13:21:34 -0500

    Logical Statement

---
+++ logicastatement.py
@@ -0,0 +1,16 @@
+Mark1=float(input("Enter your Marks for 1:"))
+Mark2=float(input("Enter your Marks for 2:"))
+Mark3=float(input("Enter your Marks for 31:"))
+Avg=(Mark1+Mark2+Mark3)/3
+if(Avg>=90 and Mark1 >= 50 and Mark2 >= 50 and Mark3 >= 50):
+    print("Excellent You got A+. Now go play Games")
+elif(Avg>=80 and Mark1 >= 50 and Mark2 >= 50 and Mark3 >= 50):
+    print("Good You got B. Now go play Games")
+elif(Avg>=70 and Mark1 >= 50 and Mark2 >= 50 and Mark3 >= 50):
+    print("Not Bad You got C. Now go play Games")
+elif(Avg>=60 and Mark1 >= 50 and Mark2 >= 50 and Mark3 >= 50):
+    print("Keep Trying You got D. Now go play Games")
+elif(Avg>=50 and Mark1 >= 50 and Mark2 >= 50 and Mark3 >= 50):
+    print("You got E. No Game for u")
+else:
+    print("You got F. No Game for u")
