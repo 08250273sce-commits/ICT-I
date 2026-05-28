3f32031b7bc9bc57bf7fb1e8ce4524bb4fd7aab0
08250273sce-commits <08250273.sce@rub.edu.bt>
2026-05-28 15:41:16 +0600

    Rename FormatMEthod.py to doneFormatMEthod.py

--- FormatMEthod.py
+++
@@ -1,1 +0,0 @@
-print("{p} is a programming language. With the help of {p}, we can create various applications.".format(p='Python'))
---
+++ doneFormatMEthod.py
@@ -0,0 +1,1 @@
+print("{p} is a programming language. With the help of {p}, we can create various applications.".format(p='Python'))
